7435080c65fcf843c5375394596fa7fc3a1d9339
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-17 15:35:09 -0600

    Testing environment established, first test passing

---
+++ api/golfCourses/golfCourses-model.test.js
@@ -0,0 +1,14 @@
+const GolfCourse = require('./golfCourses-model'); 
+const db = require('../../data/dbConfig');
+
+const MQ = { name: "Marquette" }; 
+const AG = { name: "Augusta" }; 
+const BP = { name: "Beth Page" }; 
+
+
+
+describe('Golf Course Model', () => {
+  it('Course.getAll returns empty array if no courses', async () => {
+
+  });
+});
