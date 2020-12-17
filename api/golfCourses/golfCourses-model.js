0f197c671de7e815161bad1cdb56f68d5cb3037b
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-17 15:58:42 -0600

    courses model built out and passing all tests

--- api/golfCourses/golfCourses-model.js
+++ api/golfCourses/golfCourses-model.js
@@ -2,8 +2,14 @@ const db = require('../../data/dbConfig');
 
 module.exports = {
   getAll,
+  addCourse
 };
 
 function getAll() {
   return db('courses');
+}
+
+async function addCourse(course) {
+  const [id] = await db('courses').insert(course); 
+  return db('courses').where({id}).first();
 }
--- api/golfCourses/golfCourses-model.test.js
+++ api/golfCourses/golfCourses-model.test.js
@@ -5,10 +5,28 @@ const MQ = { name: "Marquette" };
 const AG = { name: "Augusta" }; 
 const BP = { name: "Beth Page" }; 
 
-
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+beforeEach(async () => {
+  await db('courses').truncate();
+});
+afterAll(async () => {
+  await db.destroy();
+});
 
 describe('Golf Course Model', () => {
   it('Course.getAll returns empty array if no courses', async () => {
-
+    const result = await GolfCourse.getAll(); 
+    expect(result).toHaveLength(0);
+  });
+  it('GolfCourse.getAll returns courses', async () => {
+    await GolfCourse.addCourse(MQ); 
+    let result = await GolfCourse.getAll(); 
+    expect(result).toHaveLength(1);
+    expect(result[0]).toHaveProperty('id');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0]).toMatchObject(MQ); 
   });
 });
